70630f5b7736c3a4b4e222c87d53f6570391b3e0
eilon1996 <toledanoeilon3@gmail.com>
2020-10-09 15:03:35 +0300

    regestrate

--- routes/registrate.js
+++ routes/registrate.js
@@ -2,30 +2,36 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+const axios = require('axios');
+
 const database = [];
 const secret = "secret"
 
-router.post('/signup', (req, res) => {
-  var details = req.body;
+router.patch('/signup', (req, res) => {
+  console.log(JSON.stringify(req.body))
 
-  const user = database.filter(({ email }) => email === details.email);
-  if (user.length > 0) res.send(null);
-  else {
-    const token = jwt.sign(details, secret)
-    details.token = token;
-    database.push(details);
-    res.send({user:details});
-  }
+  const token = jwt.sign(req.body, secret)
+  req.body.token = token;
+
+  axios.patch(process.env.DATABASE_URL + "/content", req.body)
+    .then(res => {
+      console.log(JSON.stringify({ worked: true, req }))
+      res.send({ worked: true, req });
+    })
+    .catch(err => {
+      console.log(JSON.stringify({ worked: true, req, err }))
+      res.send({ worked: false, user:req.details, err });
+    })
 });
 
 router.post('/login-name', (req, res) => { // getting user name and password
   var details = req.body;
   const user = database.filter(({ email }) => email === details.email);
   console.log("connected with username user: ", user, "database", database);
-  if (user.length > 0 && details.password === user[0].password){
+  if (user.length > 0 && details.password === user[0].password) {
     const token = jwt.sign(user[0], secret)
-    res.send({"user":user[0], "token":token});
-  } 
+    res.send({ "user": user[0], "token": token });
+  }
   else res.send(null);
 });
 
@@ -34,9 +40,9 @@ router.post('/login-token', (req, res) => { // getting the user cookie
   var details = req.body;
   const user = database.filter(({ token }) => token === details.token);
   console.log("connected with token  user: ", user, "database", database);
-  if (user.length > 0){
-    res.send({"user":user[0]});
-  } 
+  if (user.length > 0) {
+    res.send({ "user": user[0] });
+  }
   else res.send(null);
 });
 module.exports = router;
